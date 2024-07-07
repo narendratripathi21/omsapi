6cc31a044ef0521f1405632c62abf59534c9cb7c
narendratripathi21 <narendra.tripathi@outlook.com>
2024-07-07 16:30:56 +0530

    Removed unnecessary imports

--- src/main/java/com/emerging5/omsapi/controller/ProcessController.java
+++ src/main/java/com/emerging5/omsapi/controller/ProcessController.java
@@ -4,12 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.emerging5.omsapi.service.ProcessService;
 import com.emerging5.omsapi.model.Process;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 import java.util.List;
 
 @RestController
--- src/main/java/com/emerging5/omsapi/service/ProcessService.java
+++ src/main/java/com/emerging5/omsapi/service/ProcessService.java
@@ -2,7 +2,6 @@ package com.emerging5.omsapi.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.emerging5.omsapi.model.Process;
@@ -13,7 +12,6 @@ public class ProcessService {
 
     private final ProcessRepository processRepository;
 
-    @Autowired
     public ProcessService(ProcessRepository processRepository){
         this.processRepository = processRepository;
     }
